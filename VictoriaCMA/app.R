58bcc53dc4c3ab00bdc4bfe8881e2d590e8171b3
Wendy Anthony <WendyAnthony@users.noreply.github.com>
2020-01-02 07:46:08 -0800

    Add files via upload

--- VictoriaCMA/app.R
+++ VictoriaCMA/app.R
@@ -1,9 +1,14 @@
+########################
+#  used for shiny app  #
+########################
+## To update app > Run App in RStudio, then Republish
+
 # ------------------------
 # code to get_census data ------------------------
 # https://mountainmath.github.io/cancensus/reference/get_census.html
 # code to select tmap variables ------------------------
 # https://rdrr.io/cran/tmap/man/renderTmap.html
-# code adapted - Wendy Anthony 2019-12-29
+# code adapted - Wendy Anthony 2019-12-29 / 2020-01-02
 
 # libraries needed ------------------------
 library("cancensus") # get_census
@@ -25,8 +30,8 @@ options(cancensus.api_key="CensusMapper_ec4999c4af76e21646476719765e0491")
 victoria_house_census_data <- get_census(dataset='CA16', regions=list(CMA="59935"), vectors=c("v_CA16_2246", "v_CA16_408","v_CA16_409","v_CA16_410","v_CA16_417","v_CA16_418","v_CA16_419","v_CA16_420","v_CA16_421","v_CA16_422","v_CA16_423","v_CA16_424","v_CA16_425"), level='CSD', geo_format = "sf", labels="short")
 
 # download with no vectors --------------------------
-victoria_house_census_data_simple <- get_census(dataset='CA16', regions=list(CMA="59935"), level='CSD', geo_format = "sf", labels="short")
-head(victoria_house_census_data_simple)
+# victoria_house_census_data_simple <- get_census(dataset='CA16', regions=list(CMA="59935"), level='CSD', geo_format = "sf", labels="short")
+# head(victoria_house_census_data_simple)
 
 class(victoria_house_census_data)
 # find index number for each column ------------------------
@@ -52,23 +57,31 @@ names(victoria_house_census_data) [27] <- "Avg_household_size"
 head(victoria_house_census_data)
 names(victoria_house_census_data)
 
+# to compare different polygon data, census data needs to be 'normalized'
+# by dividing by polygon population > number per person in area
+# mean/median income is already normalized
+
+
+
 # spatial transorm & reproject ------------------------
 victoria_house_census_data_sp <- as_Spatial(victoria_house_census_data)
 vic_cens <- spTransform(victoria_house_census_data_sp, CRS("+init=epsg:3005")) # bc albers
 names(vic_cens)
 class(vic_cens)
+summary(vic_cens) # 1 row NA's Esquimalt
 
 # subset with some columns removed
-vic_cens2 <- subset(vic_cens, select = c(4, 6, 7, 8, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26))
-                    
+# this is the order that summary & data table will use
+vic_cens2 <- subset(vic_cens, select = c(7, 4, 6, 8, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26))
+names(vic_cens2)                    
 head(vic_cens2)
-                    
+summary(vic_cens2)
                     
 # set a variable for column names ------------------------
 # this seems to be including all columns - use y value to list columns to get omitted
 vic_cens2_var <- setdiff(names(vic_cens2), c("Shape.Area", "Type","GeoUID",  "PR_UID", "CD_UID", "CMA_UID", "name", "Dwellings"))
 
-summary(vic_cens2)
+summary(vic_cens2_var)
 # User interface ----------------------------------
 ui <- fluidPage(
   titlePanel("Victoria CMA 2016 Census Household Visualization"),
